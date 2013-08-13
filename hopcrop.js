9fe181d00f34566fc2e868371fe44a47a941aba0
ribakker <richard@ribakker.nl>
2013-08-13 15:17:32 +0200

    made hopcrop ie compatible. moved some blocks of js around and hide textfield using .hide() instead of making it a hidden field

--- hopcrop.js
+++ hopcrop.js
@@ -1,60 +1,11 @@
 (function ($) {
 
-var HopCrop = function(context, settings) {
-      // Get the img tag where the Jcrop should be assigned to.
-  var image = $('.hopcrop-image > img', context),
-
-      // Make sure we have the elements in place to assign the navigation
-      // elements to.
-      nav = (function(c) {
-        var container = $("<div />").addClass('hopcrop-navigation').insertBefore($('.hopcrop-image', c));
-        setTimeout(function() {
-          container.height($('.hopcrop-image', c).outerHeight());
-        }, 100);
-
-        return $("<ul />").appendTo(container);
-      })(context),
-
-      // Prepare a location to store the style objects.
-      styles = {},
-
-      // Setup our public interface. This will be passed into Style and used as
-      // a return value;
-      r = {
-        nav: nav,
-        image: image,
-        deactivate: function() {
-          $.each(styles, function(n, s) { s.deactivate(); });
-        }
-      };
-
-  // Loop over every textfield.
-  $('.form-type-textfield', context).each(function() {
-    // Figure out the style name, and jump out if we can't.
-    var name = $('input[type=text]', this).attr('name'),
-        match = name.match(/\[(\w+)\]$/);
-    if (!match || (settings.styles[match[1]] === undefined)) {
-      return;
-    }
-
-    // Setup a Style for the textfield.
-    settings.styles[match[1]].display_factor = settings.display_factor;
-    styles[match[1]] = Style(r, match[1], this, settings.styles[match[1]]);
-  });
-
-  // Activate the first style. Returning false prevents the continuation of the
-  // loop.
-  $.each(styles, function(n, s) { s.activate(); return false; });
-
-  return r;
-}
-
 var Style = function(c, style_name, style_item, settings) {
       // Figure out the label of the text field and remove the element.
   var label = $('label', style_item).remove().text(),
 
       // Get the input and make it hidden.
-      input = $('input[type=text]', style_item).detach().attr('type', 'hidden').appendTo(style_item),
+      input = $('input[type=text]', style_item).hide(),
 
       // Create a navigation item.
       nav_item = $("<li />").appendTo(c.nav).append($("<a />").attr('href', '#').text(label)),
@@ -65,7 +16,7 @@ var Style = function(c, style_name, style_item, settings) {
       // Set the coordinates.
       set = function(c) {
         // If the crop spans 0px area, it's considered empty.
-        if (c.x == c.x2 && c.y == c.y2) {
+        if (c.x === c.x2 && c.y === c.y2) {
           input.val("");
           nav_item.removeClass('filled');
           return;
@@ -125,7 +76,58 @@ var Style = function(c, style_name, style_item, settings) {
   });
 
   return { deactivate: deactivate, activate: activate };
-}
+};
+
+
+var HopCrop = function(context, settings) {
+      // Get the img tag where the Jcrop should be assigned to.
+  var image = $('.hopcrop-image > img', context),
+
+      // Make sure we have the elements in place to assign the navigation
+      // elements to.
+      nav = (function(c) {
+        var container = $("<div />").addClass('hopcrop-navigation').insertBefore($('.hopcrop-image', c));
+        setTimeout(function() {
+          container.height($('.hopcrop-image', c).outerHeight());
+        }, 100);
+
+        return $("<ul />").appendTo(container);
+      }(context)),
+
+      // Prepare a location to store the style objects.
+      styles = {},
+
+      // Setup our public interface. This will be passed into Style and used as
+      // a return value;
+      r = {
+        nav: nav,
+        image: image,
+        deactivate: function() {
+          $.each(styles, function(n, s) { s.deactivate(); });
+        }
+      };
+
+  // Loop over every textfield.
+  $('.form-type-textfield', context).each(function() {
+    // Figure out the style name, and jump out if we can't.
+    var name = $('input[type=text]', this).attr('name'),
+        match = name.match(/\[(\w+)\]$/);
+    if (!match || (settings.styles[match[1]] === undefined)) {
+      return;
+    }
+
+    // Setup a Style for the textfield.
+    settings.styles[match[1]].display_factor = settings.display_factor;
+    styles[match[1]] = Style(r, match[1], this, settings.styles[match[1]]);
+  });
+
+  // Activate the first style. Returning false prevents the continuation of the
+  // loop.
+  $.each(styles, function(n, s) { s.activate(); return false; });
+
+  return r;
+};
+
 
 Drupal.behaviors.HopCrop = {
   attach: function (context, settings) {
@@ -137,6 +139,6 @@ Drupal.behaviors.HopCrop = {
       HopCrop(this, settings.HopCrop[$(this).attr('id')]);
     });
   }
-}
+};
 
-})(jQuery);
+}(jQuery));
